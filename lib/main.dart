cd0c442961f5d6fdf3ce80da679e20489e683377
ARJUN WANKHEDE <66715308+Arjun2905@users.noreply.github.com>
2022-03-02 10:18:40 +0530

    splash screen in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -7,6 +8,7 @@ import 'package:code_aware/view/screens/auth_screens/login.dart';
 import 'package:code_aware/view/screens/auth_screens/welcome.dart';
 import 'package:code_aware/view/screens/auth_screens/signup.dart';
 import 'package:code_aware/view/screens/in_app_screens/home.dart';
+import 'package:page_transition/page_transition.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -45,14 +47,30 @@ class CodeAware extends State<MyCodeAware> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: FirebaseAuth.instance.currentUser==null ? 'login_screen':'home_page',
-      routes: {
-        'welcome': (context) => WelcomeScreen(),
-        'signup_screen': (context) => SignUpScreen(),
-        'login_screen' : (context) => LogInScreen(),
-        'home_page' : (context) => HomeScreen()
-      },
-      home: WelcomeScreen(),
+      debugShowCheckedModeBanner: false,
+      // initialRoute: 'welcome',
+      // routes: {
+      //   'welcome': (context) => WelcomeScreen(),
+      //   'signup_screen': (context) => SignUpScreen(),
+      //   'login_screen' : (context) => LogInScreen(),
+      //   'home_page' : (context) => HomeScreen()
+      // },
+      home: AnimatedSplashScreen(
+          duration: 10000,
+          splashIconSize: 150,
+          splash: Image.asset("assets/images/logo.png"),
+          nextScreen: _getNextScreen(),
+          splashTransition: SplashTransition.fadeTransition,
+          pageTransitionType: PageTransitionType.fade,
+          backgroundColor: Colors.white
+      ),
     );
+  }
+
+  Widget _getNextScreen(){
+    if(FirebaseAuth.instance.currentUser==null){
+      return LogInScreen();
+    }
+    return HomeScreen();
   }
 }
